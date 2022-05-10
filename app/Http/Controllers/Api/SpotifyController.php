d5943267f0f7f74babd91f4648e52c6cd4166cb3
Matthew Knight <matt@reconstrukt.com>
2022-05-10 08:14:11 -0400

    token fix 3

--- app/Http/Controllers/Api/SpotifyController.php
+++ app/Http/Controllers/Api/SpotifyController.php
@@ -29,11 +29,11 @@ class SpotifyController extends BaseController {
     
     $scope = "streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state";$state = uniqid() . self::SEPERATOR . $client_state;
     $params = [
-      'response_type' => 'token',
+      'response_type' => 'code',
       'client_id' => config('spotify.spotify_client_id'),
       'scope' => $scope,
       'redirect_uri' => url('/api/v1/spotify/callback'),
-      'state' => $state,
+      //'state' => $state,
       'show_dialog' => $show_dialog,
     ];
     
